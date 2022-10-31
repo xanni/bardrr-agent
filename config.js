8070e323960251a6ce62d037bc7bbf3bc545d18c
Marcin Kostecki <49038365+marcinkostecki@users.noreply.github.com>
2022-10-31 10:38:18 -0700

    add some rrweb options to config and set maskAllInputs to true
    Co-authored-by: Aaron Hull <aahull@gmail.com>
    Co-authored-by: Eugene O'Fallon <epofallon@gmail.com>
    Co-authored-by: Nino Lambert <anthony.lamb001@gmail.com>

--- config.js
+++ config.js
@@ -1,6 +1,18 @@
 const config = {
   endpoint: 'http://localhost:3001',
   MAX_IDLE_TIME: 5 * 1000,
+  recordOptions: {
+    blockClass: 'rr-block',
+    blockSelector: null,
+    ignoreClass: 'rr-ignore',
+    ignoreCSSAttributes: null,
+    maskTextClass: 'rr-mask',
+    maskTextSelector: null,
+    maskAllInputs: true,
+    maskInputOptions: { password: true },
+    maskInputFn: undefined,
+    maskTextFn: undefined,
+  },
   recordConsolePlugin: {
     level: ['error'],
     lengthThreshold: Number.POSITIVE_INFINITY,
